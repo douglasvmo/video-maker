c51b6888c4bac0b3d1a269eb00ab6b4efdc998bb
Douglas Oliveira <douglas4030@gmail.com>
2020-05-11 20:23:17 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const robots = {
   input: require('./robots/input'),
   text: require('./robots/text'),
+  image: require('./robots/image'),
 };
 const state = require('./robots/state');
 
 async function start() {
   robots.input();
   await robots.text();
+  await robots.image();
 
   const content = state.load();
   console.dir(content, { depth: null });
